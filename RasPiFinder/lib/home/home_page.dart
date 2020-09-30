c22a4334bf3b83a7d495b43f22f66d3fd0eb6459
youqin sun <youqin.sun@metropolia.fi>
2020-09-30 14:27:02 +0300

    rebased on master which contains the home page

--- RasPiFinder/lib/home/home_page.dart
+++ RasPiFinder/lib/home/home_page.dart
@@ -1,26 +1,74 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class HomePage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  File _image;
+  final picker = ImagePicker();
+
+  Future getImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.camera);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-        title: Text(
-        "RasPiFinder",
-        style: TextStyle(
-        color: Colors.white,
-        letterSpacing: 2,
-        fontWeight: FontWeight.bold,
-        fontSize: 20
-    ),
+      appBar: AppBar(
+      title: Text(
+      "RasPiFinder",
+      style: TextStyle(
+          color: Colors.white,
+          letterSpacing: 2,
+          fontWeight: FontWeight.bold,
+          fontSize: 20
+      ),
     ),
     backgroundColor: Colors.blue,
     centerTitle: true,
     ),
-    backgroundColor: Colors.white,
-    body: Center(
-      child: Text("This is the dummy home page"),
-    ),
+      body: Center(
+        child: _image == null
+            ? Text('Click the Scan button to open camera',
+            style: new TextStyle(fontSize: 20, fontWeight: FontWeight.bold))
+            :Image.file(_image),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        icon:Icon(Icons.camera_alt),
+        label: Text("Scan"),
+        onPressed: getImage,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.home),
+            title: new Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.person),
+            title: new Text('Profile'),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.search),
+            title: new Text('Search'),
+          ),
+        ],
+      ),
     );
   }
 }
