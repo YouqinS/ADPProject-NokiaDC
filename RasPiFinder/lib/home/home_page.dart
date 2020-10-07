9523d8e09833f85cece4e7ddf9976767bf090a60
kyoujing <44358589+kyoujing@users.noreply.github.com>
2020-10-07 12:02:24 +0300

    Update home_page.dart

--- RasPiFinder/lib/home/home_page.dart
+++ RasPiFinder/lib/home/home_page.dart
@@ -1,7 +1,10 @@
 import 'dart:io';
 
+import 'package:RasPiFinder/components/app_bar.dart';
+import 'package:RasPiFinder/components/navigate.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:RasPiFinder/app_user/app_user.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -27,29 +30,63 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-      title: Text(
-      "RasPiFinder",
-      style: TextStyle(
-          color: Colors.white,
-          letterSpacing: 2,
-          fontWeight: FontWeight.bold,
-          fontSize: 20
+      appBar: piAppBar,
+      body: Column(children: ListTile.divideTiles(context: context, tiles: [
+      ListTile(
+          leading: Image.network(
+              "https://cdn4.iconfinder.com/data/icons/flat-brand-logo-2/512/nokia-512.png"),
+          title: Text('Product 1'),
+          subtitle: Text('1234567'),
+          trailing: Icon(Icons.chevron_right)),
+      ListTile(
+          leading: Image.network(
+              "https://cdn4.iconfinder.com/data/icons/flat-brand-logo-2/512/nokia-512.png"),
+          title: Text('Product 2'),
+          subtitle: Text('1234567'),
+          trailing: Icon(Icons.chevron_right)),
+      ListTile(
+          leading: Image.network(
+              "https://cdn4.iconfinder.com/data/icons/flat-brand-logo-2/512/nokia-512.png"),
+          title: Text('Product 3'),
+          subtitle: Text('1234567'),
+          trailing: Icon(Icons.chevron_right)),
+      ListTile(
+          leading: Image.network(
+              "https://cdn4.iconfinder.com/data/icons/flat-brand-logo-2/512/nokia-512.png"),
+          title: Text('Product 4'),
+          subtitle: Text('1234567'),
+          trailing: Icon(Icons.chevron_right)),
+      ListTile(
+          leading: Image.network(
+              "https://cdn4.iconfinder.com/data/icons/flat-brand-logo-2/512/nokia-512.png"),
+          title: Text('Product 5'),
+          subtitle: Text('1234567'),
+          trailing: Icon(Icons.chevron_right)),
+        ListTile(
+            leading: Image.network(
+                "https://cdn4.iconfinder.com/data/icons/flat-brand-logo-2/512/nokia-512.png"),
+            title: Text('Product 6'),
+            subtitle: Text('1234567'),
+            trailing: Icon(Icons.chevron_right)),
+        ListTile(
+            leading: Image.network(
+                "https://cdn4.iconfinder.com/data/icons/flat-brand-logo-2/512/nokia-512.png"),
+            title: Text('Product 7'),
+            subtitle: Text('1234567'),
+            trailing: Icon(Icons.chevron_right)),
+      ]).toList(),
       ),
-    ),
-    backgroundColor: Colors.blue,
-    centerTitle: true,
-    ),
-      body: Center(
-        child: _image == null
-            ? Text('Click the Scan button to open camera',
-            style: new TextStyle(fontSize: 20, fontWeight: FontWeight.bold))
-            :Image.file(_image),
-      ),
+
       floatingActionButton: FloatingActionButton.extended(
         icon:Icon(Icons.camera_alt),
         label: Text("Scan"),
-        onPressed: getImage,
+        tooltip: ('Open camera to scan a QR code'),
+        //TODO add function to scan qr code
+        //onPressed: getImage,
+        onPressed: () {
+          //for temporary testing
+          navigateToPage(context, AppUser());
+        },
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
 
@@ -57,15 +94,15 @@ class _HomePageState extends State<HomePage> {
         items: [
           BottomNavigationBarItem(
             icon: new Icon(Icons.home),
-            title: new Text('Home'),
+            title: new Text("Home"),
           ),
           BottomNavigationBarItem(
             icon: new Icon(Icons.person),
-            title: new Text('Profile'),
+            title: new Text("Profile"),
           ),
           BottomNavigationBarItem(
             icon: new Icon(Icons.search),
-            title: new Text('Search'),
+            title: new Text("Search"),
           ),
         ],
       ),
