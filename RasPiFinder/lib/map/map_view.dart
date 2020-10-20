cbca680b414b9600dfad786b9a4b05684af33e56
youqin sun <youqin.sun@metropolia.fi>
2020-10-21 02:00:28 +0300

    get realtime geolocation and display on map

--- RasPiFinder/lib/map/map_view.dart
+++ RasPiFinder/lib/map/map_view.dart
@@ -2,6 +2,7 @@ import 'package:RasPiFinder/components/app_bar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:geolocator/geolocator.dart';
 import "package:latlong/latlong.dart";
 
 class MapView extends StatefulWidget {
@@ -10,30 +11,57 @@ class MapView extends StatefulWidget {
 }
 
 class _MapViewState extends State<MapView> {
+  MapController controller = new MapController();
+  LatLng latLng;
+
+  Future<Position> getCurrentLocation() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    return position;
+  }
+
+  //for later user if necessary
+  Future<void> getLastKnownPosition() async {
+    await Geolocator.getLastKnownPosition();
+  }
+
+  buildMap() {
+    getCurrentLocation().then((result) => {
+    setState(() {
+    latLng = new LatLng(result.latitude, result.longitude);
+    }),
+      controller.onReady.then((value) => {
+        controller.move(new LatLng(result.latitude, result.longitude), 13.0),
+      })
+    });
+  }
+
+  @override
+  void setState(fn) {
+    if(mounted) {
+      super.setState(fn);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    var latLng = new LatLng(40.71, -74.00);
     return Scaffold(
       appBar: PiAppBar(title: 'Map View').build(context),
       body: new FlutterMap(
-        options: new MapOptions(
-          center: latLng,
-          zoom: 13.0,
-          minZoom: 10.0
-        ),
+        mapController: controller,
+        options:
+            new MapOptions(center: buildMap(), zoom: 13.0),
         layers: [
           new TileLayerOptions(
               urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-              subdomains: ['a', 'b', 'c']
-          ),
+              subdomains: ['a', 'b', 'c']),
           new MarkerLayerOptions(
             markers: [
               new Marker(
                 width: 80.0,
                 height: 80.0,
-                point: latLng,
-                builder: (ctx) =>
-                new Container(
+                point:  latLng,
+                builder: (ctx) => new Container(
                   child: Icon(
                     Icons.location_on,
                     color: Colors.blue,
