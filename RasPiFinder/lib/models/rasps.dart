cef1ff68e483c8be72a0ee5864fe28b49a229411
Nguyen Tran <kevtran07@gmail.com>
2020-11-18 12:10:49 +0200

    fixed search function breaking down due to Pi model changes

--- RasPiFinder/lib/models/rasps.dart
+++ RasPiFinder/lib/models/rasps.dart
@@ -11,20 +11,17 @@ class Rasp {
   final GeoPoint geoPoint;
 
   String getValuesString() {
-    var result = this.address + this.software;
+    var result = this.address + this.software + this.modelNumber;
     if (this.finder != null) {
       result += this.finder['email'] +
           this.finder['username'] +
           this.finder['phoneNumber'];
     }
     if (this.user != null) {
-      result +=
-          this.user['email'] + this.user['username'] + this.user['phoneNumber'];
+      result += this.user['email'] + this.user['phoneNumber'];
     }
     if (this.owner != null) {
-      result += this.owner['email'] +
-          this.owner['username'] +
-          this.owner['phoneNumber'];
+      result += this.owner['email'] + this.owner['phoneNumber'];
     }
     return result.toLowerCase();
   }
