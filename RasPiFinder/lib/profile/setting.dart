cf44d664e5c4ab40ee767aa6548e352bba4da75a
youqin sun <youqin.sun@metropolia.fi>
2020-11-22 00:57:01 +0200

    improve setting UI to show/hide fields

--- RasPiFinder/lib/profile/setting.dart
+++ RasPiFinder/lib/profile/setting.dart
@@ -18,6 +18,7 @@ class Settings extends StatefulWidget {
 class SettingsState extends State<Settings> {
   final formKey = GlobalKey<FormState>();
   String username, email, phone, password, password2;
+  bool showUsernameField = false, showEmailField = false, showPhoneField = false, showPassField = false;
 
   @override
   Widget build(BuildContext context) {
@@ -25,19 +26,28 @@ class SettingsState extends State<Settings> {
     return Scaffold(
       appBar: PiAppBar(title: 'Settings').build(context),
       body: SingleChildScrollView(
-        child: Container(
-          color: Colors.grey[100],
-          width: size.width,
-          height: size.height,
-          child: Card(
-            elevation: 0,
-            color: Colors.grey[100],
-            child:  Form(
-              key: formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height: size.height * 0.01,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
+                children: [
+                  CircleAvatar(
+                    radius: 40,
+                    backgroundColor: Colors.blue[100],
+                    child: Icon(
+                      Icons.person,
+                      color: Colors.blue[900],
+                      size: size.height * 0.1,
+                    ),
+                  ),
+
+                  SizedBox(width: size.width * 0.05,),
                   Text(
                     'Edit Profile',
                     style: TextStyle(
@@ -47,88 +57,170 @@ class SettingsState extends State<Settings> {
                       fontSize: 22,
                     ),
                   ),
-                  TextInputField(
-                    hintText: "New Username",
-                    icon: Icons.person,
-                    onSaved: (value) {
-                      username = value;
-                    },
-                    validateInput: validateUsername,
-                  ),
-                  TextInputField(
-                    hintText: "New Email",
-                    icon: Icons.email,
-                    onSaved: (value) {
-                      email = value;
-                    },
-                    validateInput: validateEmailInput,
-                  ),
-                  TextInputField(
-                    hintText: "New Phone Number",
-                    icon: Icons.phone_android,
-                    onSaved: (value) {
-                      phone = value;
-                    },
-                    validateInput: validatePhoneInput,
-                  ),
-                  PasswordInputField(
-                    hintText: 'New Password',
-                    onChanged: (value) {
-                      password = value;
-                    },
-                    onSaved: (value) {
-                      password = value;
-                    },
-                    validateInput: validatePasswdInput,
-                  ),
-                  PasswordInputField(
-                    hintText: 'Confirm Password',
-                    onSaved: (value) {
-                      password2 = value;
-                    },
-                    validateInput: (password2) => Validator.confirmPasswdInput(password, password2),
-                  ),
-                  RoundedButton(
-                    text: "Save",
-                    //TODO connect to DB to store user credentials and status
-                    press: () {
-                      submit();
-                    },
-                  ),
                 ],
               ),
-            ),
-
+              SizedBox(height: size.height * 0.05,),
+              FlatButton(onPressed: usernamePressed, child: Text('update username'), color: Colors.grey[200],),
+              Visibility(
+                visible: showUsernameField,
+                child: TextInputField(
+                  hintText: "New Username",
+                  icon: Icons.person,
+                  onSaved: (value) {
+                    username = value.trim();
+                  },
+                  validateInput: validateUsername,
+                ),
+              ),
+              FlatButton(onPressed: emailPressed, child: Text('update email'), color: Colors.grey[200],),
+              Visibility(
+                visible: showEmailField,
+                child: TextInputField(
+                  hintText: "New Email",
+                  icon: Icons.email,
+                  onSaved: (value) {
+                    email = value.trim();
+                  },
+                  validateInput: validateEmailInput,
+                ),
+              ),
+              FlatButton(onPressed: phonePressed, child: Text('update phone'), color: Colors.grey[200],),
+              Visibility(
+                visible: showPhoneField,
+                child: TextInputField(
+                  hintText: "New Phone Number",
+                  icon: Icons.phone_android,
+                  onSaved: (value) {
+                    phone = value.trim();
+                  },
+                  validateInput: validatePhoneInput,
+                ),
+              ),
+              FlatButton(onPressed: passwordPressed, child: Text('update password'), color: Colors.grey[200],),
+              Visibility(
+                visible: showPassField,
+                child: Column(
+                  children: [
+                    PasswordInputField(
+                      hintText: 'New Password',
+                      onChanged: (value) {
+                        password = value;
+                      },
+                      onSaved: (value) {
+                        password = value;
+                      },
+                      validateInput: validatePasswdInput,
+                    ),
+                    PasswordInputField(
+                      hintText: 'Confirm Password',
+                      onSaved: (value) {
+                        password2 = value.trim();
+                      },
+                      validateInput: (password2) => Validator.confirmPasswdInput(password, password2),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: size.height * 0.03),
+              Visibility(
+                visible: showPassField || showUsernameField || showPhoneField || showEmailField,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    RaisedButton(
+                      color: Colors.red[600],
+                      child: Text('Cancel',
+                          style: TextStyle(color: Colors.white)),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                    RaisedButton(
+                      color: Colors.blue[700],
+                      child: Text('Save',
+                          style: TextStyle(color: Colors.white)),
+                      onPressed: () {
+                        submit();
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: size.height * 0.02,),
+            ],
           ),
         ),
       ),
     );
   }
 
+  void usernamePressed() {
+    setState(() {
+      showUsernameField = !showUsernameField;
+    });
+
+    if(!showUsernameField) {
+      setState(() {
+        username = null;
+      });
+    }
+  }
+  void emailPressed() {
+    setState(() {
+      showEmailField = !showEmailField;
+    });
+
+    if(!showEmailField) {
+      setState(() {
+        email = null;
+      });
+    }
+  }
+  void phonePressed() {
+    setState(() {
+      showPhoneField = !showPhoneField;
+    });
+    if(!showPhoneField) {
+      setState(() {
+        phone = null;
+      });
+    }
+  }
+  void passwordPressed() {
+    setState(() {
+      showPassField = !showPassField;
+    });
+    if(!showPassField) {
+      setState(() {
+        password = null;
+        password2 = null;
+      });
+    }
+  }
   String validateEmailInput(String email) {
     if (null != email && email.isNotEmpty) {
-      return Validator.validateEmailInput(email);
+      return Validator.validateEmailInput(email.trim());
     }
     return null;
   }
 
   String validatePasswdInput(String password) {
     if (null != password && password.isNotEmpty) {
-      return Validator.validatePasswdInput(password);
+      return Validator.validatePasswdInput(password.trim());
     }
     return null;
   }
 
   String validatePhoneInput(String phone) {
     if (null != phone && phone.isNotEmpty) {
-      return Validator.validatePhoneInput(phone);
+      return Validator.validatePhoneInput(phone.trim());
     }
     return null;
   }
 
   String validateUsername(String username) {
     if (username != null && username.isNotEmpty) {
-      return Validator.validateUsername(username);
+      return Validator.validateUsername(username.trim());
     }
     return null;
   }
@@ -136,13 +228,20 @@ class SettingsState extends State<Settings> {
   void submit(){
     if(formKey.currentState.validate()){
       formKey.currentState.save();
-      //TODO get current GPS data and store to DB
       //TODO connect to DB to store user input
       print(username);
       print(phone);
       print(email);
       print(password);
+
+      if (noInput(username) &&  noInput(phone) && noInput(email) && noInput(password)) {
+        Validator.showAlert(context, 'Alert', 'No input for update', 'OK');
+      }
      // navigateToPage(context, HomePage());
     }
+  }
+
+  bool noInput(String input) {
+    return null == input || input.isEmpty;
   }
 }
